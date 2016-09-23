05a3f7968d9b1c72221befe3a3f5f9cb7ec91c9b
shkabo <bosko.stupar@gmail.com>
2016-09-23 14:20:33 +0000

    passing ip, lang, software test

--- test/parser-test.js
+++ test/parser-test.js
@@ -10,24 +10,26 @@ describe("parser test", function() {
         server.close(done);
     });
    
-   it('get header request', function(done){
-       request(server)
+  it('get header request', function(done){
+      request(server)
           .get('/useragent')
-          .set({'user-agent' : 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/49.0.2623.108 Chrome/49.0.2623.108 Safari/537.36'})
+          .set('user-agent' , 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/49.0.2623.108 Chrome/49.0.2623.108 Safari/537.36')
           .expect('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/49.0.2623.108 Chrome/49.0.2623.108 Safari/537.36', done);
-   });
+  });
+   
    it('get header request ip', function(done){
         request(server)
             .get('/ip')
-            .set({'x-forwarded-for' : '192.168.1.2'})
+            .set('x-forwarded-for', '192.168.1.2')
             .expect(/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/, done);
    });
-//   it('get header language', function(done) {
-//         request(server) 
-//             .get('/language')
-//             .set({'accept-language': 'en-GB,en-US;q=0.8,en;q=0.6'})
-//             .expect('en-GB,en-US;q=0.8,en;q=0.6', done);
-//   });
+   
+   it('get header language', function(done) {
+        request(server) 
+            .get('/language')
+            .set('accept-language', 'en-GB,en-US;q=0.8,en;q=0.6')
+            .expect('en-GB,en-US;q=0.8,en;q=0.6', done);
+   });
    
    
     
